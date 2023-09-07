1d6bd59089d564768143280a072f38892193b411
Ronaldo Lopes <innovatecodes@gmail.com>
2023-09-07 19:30:35 -0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,4 +1,4 @@
-let firstDot, secondDot, thirdDot, randomNumber, n1, n2, n3, input;
+let firstDot, secondDot, dash, randomNumber, n1, n2, n3, input;
 
 const button = document.querySelector("button");
 input = document.querySelector("input");
@@ -29,9 +29,9 @@ const validator = function () {
     }
 
     if (i === 11) {
-      thirdDot = `${secondDot}${randomNumber.toString().substring(8, i)}-`;
+      dash = `${secondDot}${randomNumber.toString().substring(8, i)}-`;
       n3 = randomNumber.toString().slice(i);
-      randomNumber = `${thirdDot}${n3}`
+      randomNumber = `${dash}${n3}`
     }
   }
   return randomNumber;
